5f78e9a13df8212a366a5bb097a43229e057131e
Luis Barrera Abarca <luis.barrera@tenpo.cl>
2023-03-15 16:07:59 -0300

    Added dependecy and add constraint to logging

--- terraform/logging.tf
+++ terraform/logging.tf
@@ -3,7 +3,7 @@ resource "google_logging_project_sink" "log_sink_cdc" {
   name = format("log_sink_cdc_%s_%s", var.bigquery_dataset_name, var.bigquery_table_name)
   project = var.gcp_project_id
   destination = format("pubsub.googleapis.com/projects/%s/topics/%s", var.gcp_project_id, google_pubsub_topic.topic_cdc.name)
-  filter = format("protoPayload.methodName=google.cloud.bigquery.v2.JobService.InsertJob AND protoPayload.resourceName = projects/%s/datasets/%s/tables/%s", var.gcp_project_id, var.bigquery_dataset_name, var.bigquery_table_name)
+  filter = format("protoPayload.methodName=google.cloud.bigquery.v2.JobService.InsertJob AND protoPayload.authorizationInfo[0].permission=bigquery.tables.updateData AND protoPayload.resourceName = projects/%s/datasets/%s/tables/%s", var.gcp_project_id, var.bigquery_dataset_name, var.bigquery_table_name)
   unique_writer_identity = true
 }
 
